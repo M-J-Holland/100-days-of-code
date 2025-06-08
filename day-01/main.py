d1adb0ba257bfd0bf913a9d845ac47fccfc5743e
Mathew <mathew_holland@hotmail.co.uk>
2025-06-08 19:11:03 +0100

    Completed the section on variables, also commented out previous code so that it doesn't run

--- day-01/main.py
+++ day-01/main.py
@@ -3,8 +3,19 @@ Everything from day one
 """
 
 # Printing
-
-print("Hello world\n")
+# print("Hello world\n")
 
 # String manipulation
-print("Hello world!\nHello world!\nHello world!\n")
+# print("Hello world!\nHello world!\nHello world!\n")
+
+# inputs + String concatenation
+# print("Hello " +input("What is your name? ") + "!")
+
+#Variables
+# name = input("What is your name? ")
+# print(name)
+
+# print(len(input("What is your name? ")))
+# name = input("What is your name? ")
+# name_length = len(name)
+# print(name_length)
