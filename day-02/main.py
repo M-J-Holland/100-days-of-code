fc7cd1a1fca776111700d456e424b607e730026b
Mathew <mathew_holland@hotmail.co.uk>
2025-06-09 20:00:01 +0100

    Day - 02: Learnt about data types

---
+++ day-02/main.py
@@ -0,0 +1,30 @@
+# data types
+
+# 1 - Strings
+# 2 - Integers
+# 3 - Floats
+# 4 - Booleans
+
+# Subscripting
+print("Hello"[0])
+
+# Print the last character in 'Hello'
+print("Hello"[4])
+print("Hello"[-1])
+
+# String
+print("123" + "345")
+
+# Integer
+print(123 + 345)
+
+# Large integer
+print(123_456_789)
+
+# Floats
+
+print(3.14)
+
+# Boolean
+print(True)
+print(False)
