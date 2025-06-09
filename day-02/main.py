fde35770071282dfd5b7633047ff4f7c07bd24f6
Mathew <mathew_holland@hotmail.co.uk>
2025-06-09 21:20:24 +0100

    learnt math operators and the pemdas rule

--- day-02/main.py
+++ day-02/main.py
@@ -6,25 +6,57 @@
 # 4 - Booleans
 
 # Subscripting
-print("Hello"[0])
+# print("Hello"[0])
 
 # Print the last character in 'Hello'
-print("Hello"[4])
-print("Hello"[-1])
+# print("Hello"[4])
+# print("Hello"[-1])
 
 # String
-print("123" + "345")
+# print("123" + "345")
 
 # Integer
-print(123 + 345)
+# print(123 + 345)
 
 # Large integer
-print(123_456_789)
+# print(123_456_789)
 
 # Floats
 
-print(3.14)
+# print(3.14)
 
 # Boolean
-print(True)
-print(False)
+# print(True)
+# print(False)
+
+# Type error, checking and conversion
+
+# print(type("Hello"))
+# print(type(1234))
+# print(type(3.14))
+# print(type(True))
+
+# Type casting
+# print(type(int("123")))
+
+# name_of_the_user = input("Enter your name: ")
+# length_of_name = len(name_of_the_user)
+# print("Number of letters in your name: " + str(length_of_name))
+# print("Number of letters in your name: " + str(len(input("Enter your name "))))
+
+# Mathematical operations
+print(123 + 456)
+print(7 - 3)
+print(3 * 2)
+print(6 / 3) # returns a floating point number
+print(6//3) # returns an integer value
+print(2 ** 3)
+
+# PEMDAS: calculates Left to Right
+# ()
+# **
+# *
+# /
+# +
+# -
+print(3 * 3 / 3 + 3 - 3)
