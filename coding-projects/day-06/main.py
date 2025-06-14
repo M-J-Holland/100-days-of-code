cd197253d25d3e1846b50473f3260928a56adff4
Mathew <mathew_holland@hotmail.co.uk>
2025-06-14 20:45:35 +0100

    Completed my own small project using a function and while loop

---
+++ coding-projects/day-06/main.py
@@ -0,0 +1,10 @@
+# This is my own project, using functions and while loops
+
+def ten_to_zero():
+    num = 10
+    while num > 0:
+        print(num)
+        num -= 1
+
+ten_to_zero()
+print("BLASTOFF!!!")
