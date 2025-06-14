571876ea44ff11ffe608623566bc2410328e5760
Mathew <mathew_holland@hotmail.co.uk>
2025-06-14 20:33:13 +0100

    Completed day 6 lessons

---
+++ days/day-06/main.py
@@ -0,0 +1,13 @@
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+num = 0
+
+while num < 5:
+    my_function()
+    num += 1
+
+# day 6 used the Reebor's world for learning functions and while loops, so In have used some of what I have learnt in
+# this main.py
