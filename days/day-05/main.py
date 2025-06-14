ef8124c8d7c000d76278c5494fbd80f7cea95ffd
Mathew <mathew_holland@hotmail.co.uk>
2025-06-14 14:34:11 +0100

    Completed day 5 lessons

---
+++ days/day-05/main.py
@@ -0,0 +1,12 @@
+# for loop
+#
+# fruits = ['Apple', 'Peach', 'Pear']
+#
+# for fruit in fruits:
+#     print(fruit)
+
+# calculating the total numbers from 1 to 100
+num_total = 0
+for num in range(1, 101):
+    num_total += num
+print(num_total)
